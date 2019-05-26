f93326af9de3bff0ad596c77fbeb08e3f47fce5b
arizon-dread <arizon.es@gmail.com>
2019-05-26 16:47:02 +0200

    initial commit

---
+++ src/main/java/com/arizon/statisticalWebClient/StatisticalWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.arizon.statisticalWebClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalWebClientApplication.class, args);
+	}
+
+}
